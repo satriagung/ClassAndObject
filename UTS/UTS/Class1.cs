1a426224a22d188e26fe1f6698c1da1627dc6cee
satriagung <48642673+satriagung@users.noreply.github.com>
2019-05-02 00:48:08 +0700

    Add files via upload

---
+++ UTS/UTS/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    class Karyawan
+    {
+        public string nik { get; set; }
+        public string nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+
+        public string nik2 { get; set; }
+        public string nama2 { get; set; }
+        public int GajiBulanan2 { get; set; }
+
+        public void infokar()
+        {
+            Console.WriteLine("1\t{0}\t{1}\t{2}", nik, nama, GajiBulanan);
+            Console.WriteLine("2\t{0}\t{1}\t{2}", nik2, nama2, GajiBulanan2);
+        }
+
+        public void bonus()
+        {
+            if (GajiBulanan < 0)
+            {
+                GajiBulanan = 0;
+            }
+            else if (GajiBulanan2 < 0)
+            {
+                GajiBulanan2 = 0;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Hore Kenaikan Gaji 10%");
+        }
+
+        public void infobon()
+        {
+            Console.WriteLine("1\t{0}\t{1}\t{2}", nik, nama, GajiBulanan*1.1 );
+            Console.WriteLine("1\t{0}\t{1}\t{2}", nik2, nama2, GajiBulanan2*1.1 );
+        }
+    }
+}
---
+++ UTS/UTS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan Karyawan = new Karyawan();
+
+            Console.WriteLine("No\tNIK\t\tNama\tGaji");
+            Karyawan.nama = "Satria";
+            Karyawan.nik = "190302123";
+            Karyawan.GajiBulanan = 3000000;
+            Karyawan.nama2 = "Ryo";
+            Karyawan.nik2 = "190302124";
+            Karyawan.GajiBulanan2 = 2000000;
+            Karyawan.infokar();
+            Karyawan.bonus();
+            Karyawan.infobon();
+
+            Console.ReadKey();
+        }
+    }
+}
